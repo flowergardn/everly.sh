e4d44e76b73d7cf2b577b90c087ea4e4e9adce77
astridlol <me@astrid.sh>
2024-03-21 18:44:28 -0400

    feat: plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,18 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 const Everly: AppType = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <script
+          defer
+          data-domain="everly.astrid.sh"
+          src="https://analytics.astrid.sh/js/script.js"
+        ></script>
+      </Head>
       <ClerkProvider
         appearance={{
           baseTheme: dark,
